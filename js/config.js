41a00dbebc4f15dad704388a1ef1da02015ab823
flect-fuchiwaki <akira.fuchiwaki@flect.co.jp>
2026-04-21 16:09:48 +0900

    github pages版に修正

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ window.APP_CONFIG = Object.freeze({
   clientId:    "e38ab014-d46d-406e-970a-a5e816f502fe",
   tenantId:    "4705d2d2-adc0-4eb4-9423-fc878bdcb170",
   authority:   "https://login.microsoftonline.com/4705d2d2-adc0-4eb4-9423-fc878bdcb170",
-  redirectUri: "https://flect.sharepoint.com/sites/CI_AE/Shared%20Documents/pptx-viewer/index.html",
+  redirectUri: "https://flect-fuchiwaki.github.io/mytool/",
   scopes:      ["Files.Read.All"],
   graphBaseUrl: "https://graph.microsoft.com/v1.0",
 
